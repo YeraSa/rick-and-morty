87d7cc284baa0ff4f13912bee692a33e2762b24d
YeraSa <sa.yeraly@gmail.com>
2022-11-30 18:40:21 +0600

    light theme changed

--- lib/ui/theme/app_themes.dart
+++ lib/ui/theme/app_themes.dart
@@ -34,34 +34,34 @@ final appThemeData = {
     brightness: Brightness.light,
     primaryColor: Colors.white,
     appBarTheme: const AppBarTheme(
-      backgroundColor: Color.fromRGBO(210, 211, 219, 0.8),
+      backgroundColor: Color.fromRGBO(3, 184, 152, 1),
     ),
     backgroundColor: const Color.fromRGBO(228, 229, 241, 0),
-    cardColor: const Color.fromRGBO(210, 211, 219, 0.8),
-    indicatorColor: const Color.fromRGBO(72, 75, 106, 1),
+    cardColor: const Color.fromRGBO(3, 184, 152, 1),
+    indicatorColor: const Color.fromRGBO(74, 74, 74, 1),
     fontFamily: 'Georgia',
     textTheme: const TextTheme(
       headline1: TextStyle(
           fontSize: 50,
           fontWeight: FontWeight.bold,
-          color: Color.fromRGBO(72, 75, 106, 1)),
+          color: Color.fromRGBO(74, 74, 74, 1)),
       headline2: TextStyle(
           fontSize: 30,
           fontWeight: FontWeight.w700,
-          color: Color.fromRGBO(72, 75, 106, 1)),
-      headline3: TextStyle(fontSize: 24, color: Color.fromRGBO(72, 75, 106, 1)),
+          color: Color.fromRGBO(74, 74, 74, 1)),
+      headline3: TextStyle(fontSize: 24, color: Color.fromRGBO(74, 74, 74, 1)),
       bodyText1: TextStyle(
           fontSize: 12,
           fontWeight: FontWeight.w200,
-          color: Color.fromRGBO(72, 75, 106, 1)),
+          color: Color.fromRGBO(74, 74, 74, 1)),
       bodyText2: TextStyle(
           fontSize: 16,
           fontWeight: FontWeight.w500,
-          color: Color.fromRGBO(72, 75, 106, 1)),
+          color: Color.fromRGBO(74, 74, 74, 1)),
       caption: TextStyle(
           fontSize: 11,
           fontWeight: FontWeight.w100,
-          color: Colors.grey),
+          color: Color.fromRGBO(74, 74, 74, 0.7)),
     ),
   ),
 };
